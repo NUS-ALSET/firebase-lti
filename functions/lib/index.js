c34e1b9eb4c8e7c11ff04c1b6638c70ace2826ef
Damien Lebrun <dinoboff@hotmail.com>
2017-08-11 16:23:17 +0100

    chore(functions): move functions in own module
    It leaves initialisation to the main script and allow to test functions without it.

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -8,14 +8,10 @@
 'use strict';
 
 const admin = require('firebase-admin');
-const functions = require('firebase-functions');
 
-const server = require('./server');
 const database = require('./database');
+const functions = require('./functions');
 
 admin.initializeApp(database.config());
 
-exports.server = functions.https.onRequest(server);
-
-exports.verify = functions.database.ref('provider/launches/{consumerKey}/{linkId}/users/{userId}/solution')
-  .onWrite(event => database.launches.gradeSolution(event.data, event.params));
+module.exports = functions;
